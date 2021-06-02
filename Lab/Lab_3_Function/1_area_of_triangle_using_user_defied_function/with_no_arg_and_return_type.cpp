b621952d2f28074d836bcf97df4242af44bab901
Sushil-kafle <sushil.kafle@lec.edu.np>
2021-06-02 16:38:49 +0545

    no arg but return type

--- Lab/Lab_3_Function/1_area_of_triangle_using_user_defied_function/with_no_arg_and_return_type.cpp
+++ Lab/Lab_3_Function/1_area_of_triangle_using_user_defied_function/with_no_arg_and_return_type.cpp
@@ -12,11 +12,11 @@ int main()
 float area_of_triangle()
 {
     float a, b, c, s, area;
-    cout << "Enter side of triangle: " << endl;
+    cout << "Enter side of triangle:" << endl;
     cin >> a;
-    cout << "Enter side of triangle: " << endl;
+    cout << "Enter side of triangle:" << endl;
     cin >> b;
-    cout << "Enter side of triangle: " << endl;
+    cout << "Enter side of triangle:" << endl;
     cin >> c;
     s = (a + b + c) / 2;
     area = sqrt(s * (s - a) * (s - b) * (s - c));
